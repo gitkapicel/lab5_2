f04b417df17bcbc82b36621d76797ecc2746a8d0
Lukasz Tracz <kapicel20wp.pl>
2015-05-13 23:10:22 +0200

    Naprawienie testu ktory byl spowodowany brakiem usuwanie osoby przed zakonczeniem testu.

--- src/test/java/edu/iis/mto/integrationtest/repository/PersonRepositoryIntegrationTest.java
+++ src/test/java/edu/iis/mto/integrationtest/repository/PersonRepositoryIntegrationTest.java
@@ -6,29 +6,37 @@ import static org.junit.Assert.assertEquals;
 import java.util.List;
 
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import edu.iis.mto.integrationtest.model.Person;
 
 
 public class PersonRepositoryIntegrationTest extends IntegrationTest {
 
-	
+	@Autowired
 	private PersonRepository personRepository;
 
-	@Test
-	public void testCanAccessDbAndGetTestData() {
-//		List<Person> foundTestPersons = personRepository.findAll();
-//		assertEquals(2, foundTestPersons.size());
-	}
+	
 
 	@Test
 	public void testSaveNewPersonAndCheckIsPersisted() {
-//		long count = personRepository.count();
-//		personRepository.save(a(person().withId(count + 1)
-//				.withFirstName("Roberto").withLastName("Mancini")));
-//		assertEquals(count + 1, personRepository.count());
-//		assertEquals("Mancini", personRepository.findOne(count + 1)
-//				.getLastName());
+        long count = personRepository.count();
+		
+		Person person = a(person().withId(count + 1)
+				.withFirstName("Roberto").withLastName("Mancini"));
+		personRepository.save( person );
+		assertEquals(count + 1, personRepository.count());
+		assertEquals("Mancini", personRepository.findOne(count + 1)
+				.getLastName());
+		
+		personRepository.delete( person );
+	}
+	
+	
+	@Test
+	public void testCanAccessDbAndGetTestData() {
+		List<Person> foundTestPersons = personRepository.findAll();
+		assertEquals(2, foundTestPersons.size());
 	}
 
 	private Person a(PersonBuilder builder) {
