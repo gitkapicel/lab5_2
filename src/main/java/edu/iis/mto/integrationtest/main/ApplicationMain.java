b7572fe849a5511f37bf8b05b0c1d682088924ee
Lukasz Tracz <kapicel20wp.pl>
2015-05-13 23:07:35 +0200

    kONFIGURACJA PROJEKTU

--- src/main/java/edu/iis/mto/integrationtest/main/ApplicationMain.java
+++ src/main/java/edu/iis/mto/integrationtest/main/ApplicationMain.java
@@ -1,34 +1,41 @@
 package edu.iis.mto.integrationtest.main;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import edu.iis.mto.integrationtest.config.ApplicationConfig;
+import edu.iis.mto.integrationtest.model.Person;
+import edu.iis.mto.integrationtest.repository.PersonRepository;
 import edu.iis.mto.integrationtest.utils.Mode;
 import edu.iis.mto.integrationtest.utils.ModeUtils;
 
 
 public class ApplicationMain {
 
-//    private static ApplicationContext applicationContext;
+    private static ApplicationContext applicationContext;
 //
     private static final Logger LOGGER = LoggerFactory.getLogger(ApplicationMain.class);
 
     public static void main(String[] args) {
-        /* Ustawienie trybu dzia³ania aplikacji */
+        /* Ustawienie trybu dziaÂ³ania aplikacji */
         ModeUtils.setMode(Mode.DEV);
         LOGGER.debug("Application mode set to ", Mode.DEV);
-//
-//        /* Konfiguracja kontekstu Spring i pobranie z kontenera bean'a reprezentujacego repozytorium danych*/
-//        applicationContext = new AnnotationConfigApplicationContext(ApplicationConfig.class);
-//        PersonRepository repository = applicationContext.getBean(PersonRepository.class);
-//
-//        /* Wykonanie operacji na danych */
-//        /* Wyszukanie rekordu */
-//        Person foundDevPerson = repository.findOne(1L);
-//        LOGGER.debug("Found person is: {}", foundDevPerson);
-//
-//        /* Pobranie liczby rekordów w repozytorium */
-//        long recordsNumber = repository.count();
-//        LOGGER.debug("Found {} records in total", recordsNumber);
+
+        /* Konfiguracja kontekstu Spring i pobranie z kontenera bean'a reprezentujacego repozytorium danych*/
+        applicationContext = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+        PersonRepository repository = applicationContext.getBean(PersonRepository.class);
+
+        /* Wykonanie operacji na danych */
+        /* Wyszukanie rekordu */
+        Person foundDevPerson = repository.findOne(1L);
+        LOGGER.debug("Found person is: {}", foundDevPerson);
+
+        /* Pobranie liczby rekordÃ³w w repozytorium */
+        long recordsNumber = repository.count();
+        LOGGER.debug("Found {} records in total", recordsNumber);
     }
 }
